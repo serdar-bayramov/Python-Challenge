685931cdc0947730fdb99b086f8e145d997b3775
Serdar Bayramov <85737622+serdar-bayramov@users.noreply.github.com>
2021-07-07 16:23:18 +0100

    optimised the code and change most part of it

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,10 @@
+"""This python script reads lection_data.csv and finds the names of candidates, stores them in a list
+and counts their votes, thus finds the winner. The results are output as ResultPyPoll1.txt """
+
 import os
 import csv
 import operator
+import numpy as np 
 
 #add path for budget_data.csv file
 poll_csv = os.path.join('.', 'Resources', 'election_data.csv')
@@ -11,66 +15,46 @@ with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
-    list_VoterID = []
-    list_County = []
-    list_Candidate = []
-    winner = {}
+# create a list and dictionary to store data from csv file
+    my_list = []
+    my_dict = {}
 
+# loop through csv, extract candidate names and store them in a list
     for row in csvreader:
-        list_VoterID.append(row[0])
-        list_County.append(row[1])
-        list_Candidate.append(row[2])
+        my_list.append(row[2])
+
+# using the following line of code we can find the unique values in a list and sums its values
+# this way we double check if there are other names that exist in the csv file
+    result = list(zip(*np.unique(my_list, return_counts=True)))
+
+#move results to dictionary as a value
+    my_dict = dict(result)
+    total_poll = len(my_list)
     
-    counter1 = 0
-    counter2 = 0
-    counter3 = 0
-    counter4 = 0
+    max_value = 0
+    max_key = None
 
-    for cand in list_Candidate:
-        if cand == "Khan":
-            counter1 = counter1 + 1
-    Khan_poll = counter1
+#find max value in a dictionary
+    for max in my_dict:
+        if my_dict[max] > max_value:
+            max_value = my_dict[max]
+            max_key = max
+
+#assign winner name 
+    winner_name = max_key
     
-    for cand in list_Candidate:
-        if cand == "Correy":
-            counter2 = counter2 + 1
-    Correy_poll = counter2
-
-    for cand in list_Candidate:    
-        if cand == "Li":
-            counter3 = counter3 + 1
-    Li_poll = counter3
-        
-    for cand in list_Candidate:
-        if cand == "O'Tooley":
-            counter4 = counter4 + 1
-    OTooley_poll = counter4
-
-
-    total_poll = Khan_poll + Correy_poll + Li_poll + OTooley_poll
-
-    Khan_percentage = round(int(Khan_poll)/int(total_poll) * 100,3)
-    Correy_percentage = round(int(Correy_poll)/int(total_poll) * 100,3)
-    Li_percentage = round(int(Li_poll)/int(total_poll) * 100,3)
-    OTooley_percentage = round(int(OTooley_poll)/int(total_poll) * 100,3)
-    
-    winner["Khan"]=Khan_poll
-    winner["Correy"]=Correy_poll
-    winner["Li"]=Li_poll
-    winner["O'Tooley"] = OTooley_poll
-    winner_poll = max(winner.items(), key=operator.itemgetter(1))[0]
-    
-
+#print the results
     TitleResult = "Election Results \n--------------------------"
     TotalVotes = "\nTotal votes: " + str(total_poll) + "\n--------------------------"
-    KhanVotes = "\nKhan: " + str(Khan_percentage) + "% " + "(" + str(Khan_poll) + ")"
-    CorreyVotes = "\nCorrey: " + str(Correy_percentage) + "% " + "(" + str(Correy_poll) + ")"
-    LiVotes = "\nLi: " + str(Li_percentage) + "% " + "(" + str(Li_poll) + ")"
-    OTooleyVotes = "\nO'Tooley: " + str(OTooley_percentage) + "% " + "(" + str(OTooley_poll) + ")" + "\n--------------------------"
-    Winner = "\nWinner: " + str(winner_poll) + "\n--------------------------"
+    KhanVotes = "\nKhan: " + str(round(my_dict['Khan']/total_poll * 100,3)) + "% " + "(" + str(my_dict['Khan']) + ")"
+    CorreyVotes = "\nCorrey: " + str(round(my_dict['Correy']/total_poll * 100,3)) + "% " + "(" + str(my_dict['Correy']) + ")"
+    LiVotes = "\nLi: " + str(round(my_dict['Li']/total_poll * 100,3)) + "% " + "(" + str(my_dict['Li']) + ")"
+    OTooleyVotes = "\nCorrey: " + str(round(my_dict["O'Tooley"]/total_poll * 100,3)) + "% " + "(" + str(my_dict["O'Tooley"]) + ")" + "\n--------------------------"
+    Winner = "\nWinner: " + str(winner_name) + "\n--------------------------"
 
     ResultPrint = TitleResult + TotalVotes + KhanVotes + CorreyVotes + LiVotes + OTooleyVotes + Winner
 
+#output to csv file
 txt_output = os.path.join(".", "Analysis", "ResultPyPoll.txt")
 with open(txt_output, 'w') as textfile:
     textfile.write(ResultPrint)
